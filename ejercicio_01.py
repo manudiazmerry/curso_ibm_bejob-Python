ba38c432ea7da38bda52ae5b90d4c73eb1f5e06c
Manu_Diaz-Merry <manudiazmerry@gmail.com>
2024-05-24 02:16:03 +0200

    primera version

---
+++ ejercicio_01.py
@@ -0,0 +1,30 @@
+# comentario
+print("Hola mundo")
+
+txt="""primera linea
+    segunda linea"""
+
+print(txt)
+
+
+
+gs_txt="hola" #variable global tipo string
+gn_var=7 #variable global tipo numerica
+
+ls_txt="hola" #variable local tipo string
+ln_var=3 #variable local tipo numerica
+
+CONSTANTE=3.14
+
+is_real=True #variable booleana
+
+# True==1 y False==0 
+
+# comentar control+ç
+
+# multicursor
+# control+alt y flechita
+# control+alt y flechita
+# control+alt y flechita
+# control+alt y flechita
+# control+alt y flechita
